02757aa78a09cf4c5546383c8cb256c2294ed7fe
gerleidson <gerleidson.bomfim@gmail.com>
2023-03-20 10:04:15 -0300

    Proximos Numeros Pares

---
+++ Proximos numeros pares.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+		int Num, Cont=1, QtPar=0,QtImp=0;
+		
+	    printf("Informe um numero: ");
+		scanf("%d",&Num);
+		
+		if(Num % 2 == 0){
+		
+	        while(Cont<=10){
+	        	
+	        	Num = Num + 2;
+	        	printf("\n %d", Num);
+	        	
+	        	Cont++;
+	
+		
+		
+}
+}
+			else {
+				
+				Num = Num + 1;
+				
+				while(Cont<=10){
+				
+				printf("\n %d", Num);
+	        	
+	        	Num = Num + 2;
+	        
+	        	
+	        	Cont++;
+	        }
+				
+			}
+}
